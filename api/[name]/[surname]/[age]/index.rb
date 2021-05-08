485b53b794dc871c859d70e0e9a3e94f9ac09e4a
Giacomo Rossetto <jackyman_cs4@live.it>
2021-05-08 16:13:21 +0200

    New routes

--- api/[name]/[surname]/[age]/index.rb
+++ api/[name]/[surname]/[age]/index.rb
@@ -2,8 +2,10 @@ require 'cowsay'
 
 Handler = Proc.new do |req, res|
   name = req.query['name'] || 'World'
+  surname = req.query['surname'] || 'Mr'
+  age = req.query['age'] || '21'
 
   res.status = 200
   res['Content-Type'] = 'text/html; charset=UTF-8'
-  res.body = Cowsay.say("Hello #{name}", 'cow')
+  res.body = Cowsay.say("Hello #{name} #{surname} #{age}", 'cow')
 end
--- api/index.rb
+++ api/index.rb
@@ -4,6 +4,6 @@ Handler = Proc.new do |req, res|
   name = req.query['name'] || 'World'
 
   res.status = 200
-  res['Content-Type'] = 'text/text; charset=utf-8'
+  res['Content-Type'] = 'text/html; charset=UTF-8'
   res.body = Cowsay.say("Hello #{name}", 'cow')
 end
